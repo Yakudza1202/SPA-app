b5994724e4d18a7a1aa8dcfe3e9218fc3e20b0db
Jama1202 <example@gmail.com>
2022-11-22 12:30:44 +0300

    I made this SPA project based on the video.

---
+++ src/pages/Home.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { getAllCategories } from "../api";
+import CategoryList from "../components/CategoryList";
+import Loader from "../components/Loader";
+import Search from "../components/Search";
+
+export default function Home() {
+  const [catalog, setCatalog] = useState([]);
+  const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+  const { pathname, search } = useLocation();
+  const { push } = useHistory();
+
+  const handleSearch = (str) => {
+    setFilteredCatalog(
+      catalog.filter((item) =>
+        item.strCategory.toLowerCase().includes(str.toLowerCase())
+      )
+    );
+    push({
+      pathname,
+      search: `?search=${str}`,
+    });
+  };
+
+  useEffect(() => {
+    getAllCategories().then((data) => {
+      setCatalog(data.categories);
+      setFilteredCatalog(
+        search
+          ? data.categories.filter((item) =>
+              item.strCategory
+                .toLowerCase()
+                .includes(search.split("=")[1].toLowerCase())
+            )
+          : data.categories
+      );
+    });
+  }, [search]);
+
+  return (
+    <>
+      <Search cb={handleSearch} />
+      {!catalog.length ? (
+        <Loader />
+      ) : (
+        <CategoryList catalog={filteredCatalog} />
+      )}
+    </>
+  );
+}
